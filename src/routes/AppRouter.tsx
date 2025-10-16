6ac727ee6a52d202cb39276f17a6fe53c3b55376
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-10-16 23:44:56 +0500

    Remove redundant lines

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,10 +3,7 @@ import HomePage from '../pages/HomePage'
 import LoginPage from '../pages/auth/LoginPage.tsx'
 import SuperAdminPage from '../pages/superadmin/SuperAdminPage.tsx'
 import AdminMainPage from "../pages/admin/AdminMainPage.tsx";
-<<<<<<< HEAD
 import HackathonAdminPage from "../pages/admin/HackathonAdminPage.tsx";
-=======
->>>>>>> develop
 import HackathonRegisterPage from "../pages/hackathon/HackathonRegisterPage.tsx";
 
 export const AppRouter = () => {
@@ -18,10 +15,7 @@ export const AppRouter = () => {
                 <Route path="/superadmin" element={ <SuperAdminPage />} />
                 <Route path="/admin" element={ <AdminMainPage />} />
                 <Route path="/hackathon/register" element={ <HackathonRegisterPage />} />
-<<<<<<< HEAD
                 <Route path="/admin/hackathon" element={ <HackathonAdminPage />} />
-=======
->>>>>>> develop
             </Routes>
         </BrowserRouter>
     )
