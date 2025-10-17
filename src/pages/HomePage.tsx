8f3d57046c2ab4139b2fa7c7ff4f150ed3643dd6
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-10-17 15:29:24 +0500

    Remove hackathon banner

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,6 @@
 ﻿import {MainLayout} from "../components/Layouts/MainLayout/MainLayout.tsx";
 import {HeroSection}  from "../components/HomePage/HeroSection.tsx";
 import {BigTextSection} from "../components/HomePage/BigTextSection.tsx";
-import {AnnouncementTickerContainer, LeftToRightTicker, RightToLeftTicker} from "../components/HomePage/AnnouncementTicker.tsx";
-import {HackathonRegistrationBanner} from "../components/HomePage/HackathonRegistrationBanner.tsx";
 import {AboutUsSection} from "../components/HomePage/AboutUsSection.tsx";
 import {EventsSection} from "../components/HomePage/EventsSection.tsx";
 import BoardMembersSection from "../components/HomePage/BoardMembersSection.tsx";
@@ -13,13 +11,6 @@ export default function HomePage() {
             <main>
                 <HeroSection />
                 <BigTextSection />
-                <AnnouncementTickerContainer>
-                    <LeftToRightTicker />
-                    <HackathonRegistrationBanner />
-                    <div className="mt-4">
-                        <RightToLeftTicker />
-                    </div>
-                </AnnouncementTickerContainer>
                 <AboutUsSection />
                 <EventsSection />
                 <BoardMembersSection />
