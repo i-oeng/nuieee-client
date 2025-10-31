5932d97602625cceaff0cddb482674d458f527b1
canadian <daulet.issayev@nu.edu.kz>
2025-10-31 13:27:51 +0500

    HeroSection is now centered and clipped properly

--- src/components/HomePage/HeroSection.tsx
+++ src/components/HomePage/HeroSection.tsx
@@ -2,15 +2,14 @@
 
 export function HeroSection() {
     return (
-        <section className="relative w-full min-h-[calc(100svh-112px)] lg:min-h-[calc(100svh-148px)] bg-black overflow-hidden">
-            {/* The subtracted values come from header and padding: 64+48=112 and lg:100+48=148 */}
+        <section className="relative w-full min-h-[calc(100svh-112px)] lg:min-h-[calc(100svh-148px)] bg-black ">
             {/* Background blur effect */}
-            <div className="absolute inset-0 flex items-center justify-center">
-                <div className="w-[1165px] h-[662px] bg-blue-500/25 blur-[125px]"></div>
-            </div>
+              <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+                <div className="w-[1165px] h-[662px] bg-blue-500/25 blur-[125px]" />
+              </div>
 
             {/* Main Content */}
-            <div className="relative z-10 flex flex-col items-center justify-center min-h-[calc(100svh-112px)] lg:min-h-[calc(100svh-148px)] px-6">
+            <div className="relative z-10 flex flex-col items-center justify-center min-h-[calc(100svh-112px)] lg:min-h-[calc(100svh-148px)]">
                 <div className="-translate-y-[56px] lg:-translate-y-[74px] flex flex-col items-center text-center gap-2">
                     {/* The contents need to be lifted by 112/2=56 and lg:114/2=74 to place them at the center*/}
                     <img src={ieeeHeroSection} alt={'IEEE Hero Section'} className="block max-w-full h-auto m-0" />
